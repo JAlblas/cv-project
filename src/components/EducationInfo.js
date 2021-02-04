1a43a1da2b3b340b9342357036cc0861c00e6428
Jasper Alblas <just_jasper@hotmail.com>
2021-02-04 14:36:17 +0100

    initial project setup

---
+++ src/components/EducationInfo.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class EducationInfo extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          };
+      
+    }
+
+    render() {
+        return (
+            <div>
+                EducationInfo
+            </div>
+        )
+    }
+}
+
+export default EducationInfo
---
+++ src/components/PracticalInfo.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class PracticalInfo extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          };
+      
+    }
+
+    render() {
+        return (
+            <div>
+                PracticalInfo
+            </div>
+        )
+    }
+}
+
+export default PracticalInfo
